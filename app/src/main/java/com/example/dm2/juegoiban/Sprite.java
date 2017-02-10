96595949a5b25e80165eb8cd5dc2c5dbce4c6e06
ibanC <iban@yopmail.com>
2017-02-10 13:15:23 +0100

    animacion en angulos diferentes

--- app/src/main/java/com/example/dm2/juegoiban/Sprite.java
+++ app/src/main/java/com/example/dm2/juegoiban/Sprite.java
@@ -11,7 +11,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
+import java.util.Random;
+
 public class Sprite {
+    int [] DIRECTION_TO_ANIMATION_MAP = {3, 1, 0, 2};
     private static final int BMP_ROWS = 5;
     private static final int BMP_COLUMNS = 7;
     private int x = 0;
@@ -22,32 +25,46 @@ public class Sprite {
     private int currentFrame = 0;
     private int width;
     private int height;
+    private int ySpeed;
 
     public Sprite(VistaJuego vistaJuego,Bitmap bmp) {
         this.vistaJuego = vistaJuego;
         this.bmp = bmp;
         this.width = bmp.getWidth() / BMP_COLUMNS;
         this.height = bmp.getHeight() / BMP_ROWS;
+        Random rnd = new Random(System.currentTimeMillis());
+        xSpeed = rnd.nextInt(10) - 5;
+        ySpeed = rnd.nextInt(10) - 5;
     }
 
     private void update(){
-        if (x > vistaJuego.getWidth() - width - xSpeed) { //Para que llegue al final
-            xSpeed = -5;
-        }
-        if (x + xSpeed < 0) {
-            xSpeed = 5;
+        if (x > vistaJuego.getWidth() - width - xSpeed || x + xSpeed <= 0) { //Para que llegue al final
+            xSpeed = -xSpeed;
         }
         x = x + xSpeed;
+        if (y > vistaJuego.getHeight() - height - ySpeed || y + ySpeed <= 0) {
+            ySpeed = -ySpeed;
+        }
+        y = y + ySpeed;
+
+
         currentFrame = ++currentFrame % BMP_COLUMNS;
     }
 
     public void onDraw (Canvas canvas){
         update();
         int srcX = currentFrame * width;
-        int srcY = 1 * height;
+        int srcY = getAnimationRow() * height;
         Rect src = new Rect (srcX, srcY, srcX + width, srcY + height);
         Rect dst = new Rect (x, y, x + width, y + height);
         canvas.drawBitmap(bmp, src, dst, null);
         //canvas.drawBitmap(bmp, x, 10, null);
+    }
+    //direction = 0 up, 1 left, 2 down, 3 right
+    //animation = 3 back, 1 left, 0 front, 2 right
+    private int getAnimationRow(){
+        double dirDouble = (Math.atan2(xSpeed, ySpeed)/ (Math.PI / 2)+ 2 );
+        int direction = (int)Math.round(dirDouble) % BMP_ROWS;
+        return DIRECTION_TO_ANIMATION_MAP[direction];
     }
 }
