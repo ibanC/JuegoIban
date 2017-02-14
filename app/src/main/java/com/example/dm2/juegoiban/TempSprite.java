4ae14d49b99798f3dd5c236d3110593fe2cbb0a6
ibanC <iban@yopmail.com>
2017-02-14 16:25:18 +0100

    con cambios

---
+++ app/src/main/java/com/example/dm2/juegoiban/TempSprite.java
@@ -0,0 +1,42 @@
+package com.example.dm2.juegoiban;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.List;
+
+/**
+ * Created by dm2 on 06/02/2017.
+ */
+
+public class TempSprite {
+
+    private float x;
+    private float y;
+    private Bitmap bmp;
+    private int life = 15;
+    private List<TempSprite> temps;
+
+    public TempSprite(List<TempSprite> temps, VistaJuego vistaJuego,
+                      float x, float y, Bitmap bmp) {
+
+        this.x = Math.min(Math.max(x - bmp.getWidth() /2, 0),
+                vistaJuego.getWidth() - bmp.getWidth());
+        this.y = Math.min(Math.max(y - bmp.getHeight() /2,0),
+                vistaJuego.getHeight() - bmp.getHeight());
+        this.temps = temps;
+        this.bmp = bmp;
+    }
+
+    public void onDraw (Canvas canvas){
+        update();
+        canvas.drawBitmap(bmp, x, y, null);
+    }
+
+    private void update (){
+        if (--life < 1){
+            temps.remove(this);
+        }
+    }
+
+}
